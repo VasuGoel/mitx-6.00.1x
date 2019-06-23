35cf5599c76241b5649c1e639c3a53fc2d69686c
Vasu Goel <goelvasu0712@gmail.com>
2019-06-23 18:24:18 +0530

    Create analyze_song_lyrics.py to Lecture Programs

---
+++ Lecture Programs/analyze_song_lyrics.py
@@ -0,0 +1,13 @@
+"""
+Created on Sun Jun 23 18:18 2019
+
+@author: Vasu Goel
+"""
+
+def loadlyrics():
+    """
+    Returns a list of all words in the lyrics.
+
+    Depending on the size of the lyics, this function may
+    take a while to finish.
+    """
