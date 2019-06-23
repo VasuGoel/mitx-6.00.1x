a3e8cb59d7c0a4040940d358e9cd1daa85185552
Vasu Goel <goelvasu0712@gmail.com>
2019-06-23 18:36:19 +0530

    Add welcome function and loadlyrics()

--- Lecture Programs/analyze_song_lyrics.py
+++ Lecture Programs/analyze_song_lyrics.py
@@ -4,6 +4,8 @@ Created on Sun Jun 23 18:18 2019
 @author: Vasu Goel
 """
 
+LYRICS_FILENAME = "lyrics.txt"
+
 def loadlyrics():
     """
     Returns a list of all words in the lyrics.
@@ -11,3 +13,17 @@ def loadlyrics():
     Depending on the size of the lyics, this function may
     take a while to finish.
     """
+
+
+def welcome():
+    msg = '''
+Welcome to the song lyrics analyzer!
+
+Instructions: Paste the lyrics of the song you wish to analyze in a new file 'lyrics.txt' inside the same directory as this program.
+    '''
+    return msg
+
+
+print(welcome())
+input('Press \'Enter\' after you\'ve created \'lyrics.txt\'...')
+print("\nLoading lyrics from file ...")
