fd7accc5c60aa932895032dc3215c1e35aecc985
Vasu Goel <goelvasu0712@gmail.com>
2019-07-08 23:07:06 +0530

    Create assertions.py form lecture 8

---
+++ Lecture Programs/assertions.py
@@ -0,0 +1,9 @@
+def grader(marks):
+    """
+    marks: list of marks of students in integer
+
+    Return average marks of students
+    """
+
+    # raise AssertionError if list is empty
+    assert not len(marks) == 0, 'list cannot be empty'
