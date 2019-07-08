352ce29e85e116432c5070204aaadec1e16c09c4
Vasu Goel <goelvasu0712@gmail.com>
2019-07-08 23:14:27 +0530

    raise AssertionError if list is empty

--- Lecture Programs/assertions.py
+++ Lecture Programs/assertions.py
@@ -6,4 +6,17 @@ def grader(marks):
     """
 
     # raise AssertionError if list is empty
-    assert not len(marks) == 0, 'list cannot be empty'
+    try:
+        assert not len(marks) == 0, 'list cannot be empty'
+    except AssertionError as ae:
+        print(f'AssertionError: {ae}')
+        return 0.0
+    else:
+        sum = 0
+        for m in marks:
+            sum += m
+        return sum/len(marks)
+
+# marks = [98, 87, 94, 85, 100]
+marks = []
+print(grader(marks))
