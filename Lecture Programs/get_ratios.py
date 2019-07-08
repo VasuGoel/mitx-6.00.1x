43eee8e9c190983bebf30a08469a4c6c73fe3557
Vasu Goel <goelvasu0712@gmail.com>
2019-07-08 19:01:00 +0530

    Add get_ratios.py from lecture 8

--- Lecture Programs/get_ratios.py
+++ Lecture Programs/get_ratios.py
@@ -5,4 +5,15 @@ def get_ratios(L1, L2):
     Return list of containing L1[i]/L2[i]
     """
     ratios = []
-    for i in range(len(L1))
+    for i in range(len(L1)):
+        try:
+            ratios.append(L1[i]/float(L2[i]))
+        except ZeroDivisionError:
+            ratios.append(float('NaN'))  # Appends nan i.e. not a number
+        except:
+            raise ValueError('get_ratios called with bad arguments')
+    return ratios
+
+# print(get_ratios([1, 2, 3, 4, 5], [6, 7, 8, 9, 0])) prints [0.16666666666666666, 0.2857142857142857, 0.375, 0.4444444444444444, nan]
+
+print(get_ratios([1, 2, 3], [4, 's', 6]))
