8ff045d06e0de820db1cc51acbdbc0ab570f81f5
Vasu Goel <goelvasu0712@gmail.com>
2019-07-12 21:27:43 +0530

    Add int_set.py from lecture 9 exercises

---
+++ Exercises/int_set.py
@@ -0,0 +1,45 @@
+class intSet(object):
+    """An intSet is a set of integers
+    The value is represented by a list of ints, self.vals.
+    Each int in the set occurs in self.vals exactly once."""
+
+    def __init__(self):
+        """Create an empty set of integers"""
+        self.vals = []
+
+    def insert(self, e):
+        """Assumes e is an integer and inserts e into self"""
+        if not e in self.vals:
+            self.vals.append(e)
+
+    def member(self, e):
+        """Assumes e is an integer
+           Returns True if e is in self, and False otherwise"""
+        return e in self.vals
+
+    def remove(self, e):
+        """Assumes e is an integer and removes e from self
+           Raises ValueError if e is not in self"""
+        try:
+            self.vals.remove(e)
+        except:
+            raise ValueError(str(e) + ' not found')
+
+    def __str__(self):
+        """Returns a string representation of self"""
+        self.vals.sort()
+        return '{' + ','.join([str(e) for e in self.vals]) + '}'
+
+    def intersect(self, other):
+        """Assumes other is an intSet
+           Return a new intSet containing elements that appear in both sets"""
+        mutuals = intSet()
+        for item in self.vals:
+            if item in other.vals:
+                mutuals.insert(item)
+        return mutuals
+
+    def __len__(self):
+        """Returns the length of the set.
+           This method is called by the `len` built-in function."""
+        return len(self.vals)
