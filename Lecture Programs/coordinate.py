f2e448be14860ec4bd01c6e74ecd5a385c5e5169
Vasu Goel <goelvasu0712@gmail.com>
2019-07-12 20:14:11 +0530

    Create class coordinainate extending object class

---
+++ Lecture Programs/coordinate.py
@@ -0,0 +1,18 @@
+class Coordinate(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distance(self, other_coordinate_object):
+        x_diff_sq = (self.x - other_coordinate_object.x)**2
+        y_diff_sq = (self.y - other_coordinate_object.y)**2
+        return (x_diff_sq + y_diff_sq)**0.5
+
+
+
+# Origin
+o = Coordinate(0, 0)
+# Point
+p = Coordinate(6, 9)
+
+print(f'The distance between point o and p is: {round(o.distance(p), 2)} units')
