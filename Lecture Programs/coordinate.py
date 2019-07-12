112aa6e205df06a9aeef888ec2be01ae936d31f3
Vasu Goel <goelvasu0712@gmail.com>
2019-07-12 20:30:35 +0530

    def __str__(self):

--- Lecture Programs/coordinate.py
+++ Lecture Programs/coordinate.py
@@ -8,6 +8,9 @@ class Coordinate(object):
         y_diff_sq = (self.y - other_coordinate_object.y)**2
         return (x_diff_sq + y_diff_sq)**0.5
 
+    # Python calls __str__ method when used with 'print' on your class object
+    def __str__(self):
+        return f'({self.x}, {self.y})'
 
 
 # Origin
@@ -17,3 +20,6 @@ p = Coordinate(6, 9)
 
 print(f'The distance between point o({o.x},{o.y}) and p({p.x},{p.y}) is: {round(o.distance(p), 2)} units')
 print(f'Distance - {Coordinate.distance(o, p)}')
+
+# print(p) returns <__main__.Coordinate object at 0x109c699e8> which is 'uninformative'. We define a '__str__' method in class to define what we wanna do with that print(c)
+print(p)
