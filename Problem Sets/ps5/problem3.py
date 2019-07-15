78a2223093858eb76af54d0fb607e1387f981ae9
Vasu Goel <goelvasu0712@gmail.com>
2019-07-15 19:46:22 +0530

    Implement class CiphertextMessage(Message):

--- Problem Sets/ps5/problem3.py
+++ Problem Sets/ps5/problem3.py
@@ -8,3 +8,45 @@
 #
 # __init__(self, text): Use the parent class constructor to make your code more concise.
 # decrypt_message(self): You may find the helper function is_word(wordlist, word) and the string method split() useful. Note that is_word will ignore punctuation and other special characters when considering whether a word is valid.
+
+
+class CiphertextMessage(Message):
+    def __init__(self, text):
+        '''
+        Initializes a CiphertextMessage object
+
+        text (string): the message's text
+
+        a CiphertextMessage object has two attributes:
+            self.message_text (string, determined by input text)
+            self.valid_words (list, determined using helper function load_words)
+        '''
+        Message.__init__(self, text)
+
+    def decrypt_message(self):
+        '''
+        Decrypt self.message_text by trying every possible shift value
+        and find the "best" one. We will define "best" as the shift that
+        creates the maximum number of real words when we use apply_shift(shift)
+        on the message text. If s is the original shift value used to encrypt
+        the message, then we would expect 26 - s to be the best shift value
+        for decrypting it.
+
+        Note: if multiple shifts are  equally good such that they all create
+        the maximum number of you may choose any of those shifts (and their
+        corresponding decrypted messages) to return
+
+        Returns: a tuple of the best shift value used to decrypt the message
+        and the decrypted message text using that shift value
+        '''
+        best_count = 0
+        for i in range(1,27):
+            decoded_text = self.apply_shift(26-i)
+            count = 0
+            for word in decoded_text.split():
+                if is_word(self.valid_words, word):
+                    count += 1
+            if count > best_count:
+                best_count = count
+                best_shift_value = 26-i
+        return best_shift_value, self.apply_shift(best_shift_value)
